e8f6821b8bae763d4cd65cb70232cdcf5300980c
oeway <oeway007@gmail.com>
2022-05-02 02:36:21 +0200

    use settings

--- hypha/server.py
+++ hypha/server.py
@@ -9,6 +9,7 @@ import uvicorn
 from dotenv import find_dotenv, load_dotenv
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseSettings
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 
@@ -19,6 +20,35 @@ from hypha.http import HTTPProxy
 from hypha.triton import TritonProxy
 from hypha.utils import GZipMiddleware, GzipRoute, PatchedCORSMiddleware
 from hypha.websocket import WebsocketServer
+
+
+class Settings(BaseSettings):
+    """Settings for the server."""
+    host: str = "127.0.0.1"  # "host for the hypha server"
+    port: int = 9527  # "port for the hypha server"
+    allow_origins: str = "*"  # "origins for the hypha server",
+    base_path: str = "/"  # "the base path for the server",
+    redis_uri: str = "/tmp/redis.db"  # "the URI (a URL or database file path) for the redis database",
+    reset_redis: bool = False  # "reset and clear all the data in the redis database",
+    redis_port: int = 6383  # "the port for the redis database",
+    public_base_url: str = None  # "the public base URL for accessing the server",
+    triton_servers: str = None  # "A list of comma separated Triton servers to proxy",
+    enable_server_apps: bool = False  # "enable server applications",
+    enable_s3: bool = False  # "enable S3 object storage",
+    in_docker: bool = False  # "Indicate whether running in docker (e.g. server apps will run without sandboxing)"
+    endpoint_url: str = None  # "set endpoint URL for S3",
+    endpoint_url_public: str = None  # "set public endpoint URL for S3 (if different from the local endpoint one)"
+    access_key_id: str = None  # "set access key ID for S3",
+    secret_access_key: str = None  # "set secret access key for S3",
+    apps_dir: str = "hypha-apps"  # "temporary directory for storing installed apps",
+    rdf_bucket: str = "hypha-rdfs"  # "set the bucket name for the RDF store",
+    workspace_bucket: str = (
+        "hypha-workspaces"  # "set the bucket name for the workspaces",
+    )
+    executable_path: str = (
+        "bin"  # "temporary directory for storing executables (e.g. mc, minio)"
+    )
+
 
 logging.basicConfig(stream=sys.stdout)
 logger = logging.getLogger("server")
@@ -86,32 +116,32 @@ def create_application(allow_origins) -> FastAPI:
 def start_builtin_services(
     app: FastAPI,
     store: RedisStore,
-    args: argparse.Namespace,
+    settings: Settings,
 ) -> None:
     """Set up the builtin services."""
     # pylint: disable=too-many-arguments,too-many-locals
 
     def norm_url(url):
-        return args.base_path.rstrip("/") + url
+        return settings.base_path.rstrip("/") + url
 
     WebsocketServer(store, path=norm_url("/ws"))
 
     HTTPProxy(store)
-    if args.triton_servers:
+    if settings.triton_servers:
         TritonProxy(
             store,
-            triton_servers=args.triton_servers.split(","),
-            allow_origins=args.allow_origins,
+            triton_servers=settings.triton_servers.split(","),
+            allow_origins=settings.allow_origins,
         )
     ASGIGateway(
         store,
-        allow_origins=args.allow_origins,
+        allow_origins=settings.allow_origins,
         allow_methods=ALLOW_METHODS,
         allow_headers=ALLOW_HEADERS,
         expose_headers=EXPOSE_HEADERS,
     )
 
-    @app.get(args.base_path)
+    @app.get(settings.base_path)
     async def home():
         return {
             "name": "Hypha",
@@ -130,36 +160,36 @@ def start_builtin_services(
             "workspaces": [w.dict() for w in all_workspaces],
         }
 
-    if args.enable_s3:
+    if settings.enable_s3:
         # pylint: disable=import-outside-toplevel
         from hypha.rdf import RDFController
         from hypha.s3 import S3Controller
 
         s3_controller = S3Controller(
             store,
-            endpoint_url=args.endpoint_url,
-            access_key_id=args.access_key_id,
-            secret_access_key=args.secret_access_key,
-            endpoint_url_public=args.endpoint_url_public,
-            workspace_bucket=args.workspace_bucket,
-            executable_path=args.executable_path,
+            endpoint_url=settings.endpoint_url,
+            access_key_id=settings.access_key_id,
+            secret_access_key=settings.secret_access_key,
+            endpoint_url_public=settings.endpoint_url_public,
+            workspace_bucket=settings.workspace_bucket,
+            executable_path=settings.executable_path,
         )
 
-        RDFController(store, s3_controller=s3_controller, rdf_bucket=args.rdf_bucket)
+        RDFController(store, s3_controller=s3_controller, rdf_bucket=settings.rdf_bucket)
 
-    if args.enable_server_apps:
+    if settings.enable_server_apps:
         # pylint: disable=import-outside-toplevel
         from hypha.apps import ServerAppController
 
         ServerAppController(
             store,
-            port=args.port,
-            apps_dir=args.apps_dir,
-            in_docker=args.in_docker,
-            endpoint_url=args.endpoint_url,
-            access_key_id=args.access_key_id,
-            secret_access_key=args.secret_access_key,
-            workspace_bucket=args.workspace_bucket,
+            port=settings.port,
+            apps_dir=settings.apps_dir,
+            in_docker=settings.in_docker,
+            endpoint_url=settings.endpoint_url,
+            access_key_id=settings.access_key_id,
+            secret_access_key=settings.secret_access_key,
+            workspace_bucket=settings.workspace_bucket,
         )
 
     @app.get(norm_url("/health/liveness"))
@@ -171,169 +201,40 @@ def start_builtin_services(
 
     @app.on_event("startup")
     async def startup_event():
-        await store.init(args.reset_redis)
+        await store.init(settings.reset_redis)
 
     @app.on_event("shutdown")
     def shutdown_event():
         store.get_event_bus().emit("shutdown", target="local")
 
 
-def start_server(args):
-    """Start the server."""
-    if args.allow_origins:
-        args.allow_origins = args.allow_origins.split(",")
-    else:
-        args.allow_origins = env.get("ALLOW_ORIGINS", "*").split(",")
-    application = create_application(args.allow_origins)
-    local_base_url = f"http://127.0.0.1:{args.port}/{args.base_path.strip('/')}".strip(
-        "/"
-    )
-    if args.public_base_url:
-        public_base_url = args.public_base_url.strip("/")
-    else:
-        public_base_url = local_base_url
-    store = RedisStore(
-        application,
-        public_base_url=public_base_url,
-        local_base_url=local_base_url,
-        redis_uri=args.redis_uri,
-        redis_port=args.redis_port,
-    )
+settings = Settings()
+if settings.allow_origins:
+    settings.allow_origins = settings.allow_origins.split(",")
+else:
+    settings.allow_origins = env.get("ALLOW_ORIGINS", "*").split(",")
+application = create_application(settings.allow_origins)
+local_base_url = f"http://127.0.0.1:{settings.port}/{settings.base_path.strip('/')}".strip(
+    "/"
+)
+if settings.public_base_url:
+    public_base_url = settings.public_base_url.strip("/")
+else:
+    public_base_url = local_base_url
+store = RedisStore(
+    application,
+    public_base_url=public_base_url,
+    local_base_url=local_base_url,
+    redis_uri=settings.redis_uri,
+    redis_port=settings.redis_port,
+)
 
-    start_builtin_services(application, store, args)
-    if args.host in ("127.0.0.1", "localhost"):
+start_builtin_services(application, store, settings)
+
+if __name__ == "__main__":
+    if settings.host in ("127.0.0.1", "localhost"):
         print(
             "***Note: If you want to enable access from another host, "
             "please start with `--host=0.0.0.0`.***"
         )
-    uvicorn.run(application, host=args.host, port=int(args.port))
-
-
-def get_argparser():
-    """Return the argument parser."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--host",
-        type=str,
-        default="127.0.0.1",
-        help="host for the hypha server",
-    )
-    parser.add_argument(
-        "--port",
-        type=int,
-        default=9527,
-        help="port for the hypha server",
-    )
-    parser.add_argument(
-        "--allow-origins",
-        type=str,
-        default="*",
-        help="origins for the hypha server",
-    )
-    parser.add_argument(
-        "--base-path",
-        type=str,
-        default="/",
-        help="the base path for the server",
-    )
-    parser.add_argument(
-        "--redis-uri",
-        type=str,
-        default="/tmp/redis.db",
-        help="the URI (a URL or database file path) for the redis database",
-    )
-    parser.add_argument(
-        "--reset-redis",
-        action="store_true",
-        help="reset and clear all the data in the redis database",
-    )
-    parser.add_argument(
-        "--redis-port",
-        type=int,
-        default=6383,
-        help="the port for the redis database",
-    )
-    parser.add_argument(
-        "--public-base-url",
-        type=str,
-        default=None,
-        help="the public base URL for accessing the server",
-    )
-    parser.add_argument(
-        "--triton-servers",
-        type=str,
-        default=None,
-        help="A list of comma separated Triton servers to proxy",
-    )
-    parser.add_argument(
-        "--enable-server-apps",
-        action="store_true",
-        help="enable server applications",
-    )
-    parser.add_argument(
-        "--enable-s3",
-        action="store_true",
-        help="enable S3 object storage",
-    )
-    parser.add_argument(
-        "--in-docker",
-        action="store_true",
-        help="Indicate whether running in docker (e.g. "
-        "server apps will run without sandboxing)",
-    )
-    parser.add_argument(
-        "--endpoint-url",
-        type=str,
-        default=None,
-        help="set endpoint URL for S3",
-    )
-    parser.add_argument(
-        "--endpoint-url-public",
-        type=str,
-        default=None,
-        help="set public endpoint URL for S3"
-        "(if different from the local endpoint one)",
-    )
-    parser.add_argument(
-        "--access-key-id",
-        type=str,
-        default=None,
-        help="set AccessKeyID for S3",
-    )
-    parser.add_argument(
-        "--secret-access-key",
-        type=str,
-        default=None,
-        help="set SecretAccessKey for S3",
-    )
-    parser.add_argument(
-        "--apps-dir",
-        type=str,
-        default="hypha-apps",
-        help="temporary directory for storing installed apps",
-    )
-    parser.add_argument(
-        "--rdf-bucket",
-        type=str,
-        default="hypha-rdfs",
-        help="S3 bucket for storing RDF files",
-    )
-    parser.add_argument(
-        "--workspace-bucket",
-        type=str,
-        default="hypha-workspaces",
-        help="S3 bucket for storing workspaces",
-    )
-    parser.add_argument(
-        "--executable-path",
-        type=str,
-        default="bin",
-        help="temporary directory for storing executables (e.g. mc, minio)",
-    )
-    return parser
-
-
-if __name__ == "__main__":
-    arg_parser = get_argparser()
-    opt = arg_parser.parse_args()
-    start_server(opt)
+    uvicorn.run(application, host=settings.host, port=int(settings.port))
