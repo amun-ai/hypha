be35fe9f6dc8a6b3f4f9e76c6c63a34b7801543b
Wei Ouyang <oeway007@gmail.com>
2022-05-06 12:51:42 +0200

    fix imjoy-rpc and bump version (#188)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ REQUIREMENTS = [
     "aiobotocore>=2.1.0",
     "aiofiles",
     "fastapi>=0.70.0",
-    "imjoy-rpc==0.5.11",
+    "imjoy-rpc==0.5.12",
     "msgpack>=1.0.2",
     "numpy",
     "pydantic[email]>=1.8.2",
