112ec7ee812d0504bfca216428193bde5f0602e7
oeway <oeway007@gmail.com>
2023-06-10 13:26:16 +0200

    Fix docs

--- hypha/__main__.py
+++ hypha/__main__.py
@@ -1,6 +1,9 @@
 """Main module for hypha."""
 from hypha.server import get_argparser, start_server
 
-arg_parser = get_argparser()
-opt = arg_parser.parse_args()
-start_server(opt)
+
+def main():
+    """Main function."""
+    arg_parser = get_argparser()
+    opt = arg_parser.parse_args()
+    start_server(opt)
