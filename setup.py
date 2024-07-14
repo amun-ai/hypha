b78ae2b287bb81afbab04999b0d4681386bf1866
Wei Ouyang <oeway007@gmail.com>
2024-07-14 00:15:58 -0700

    Upgrade imjoy-rpc to 0.5.57

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ DESCRIPTION = (
 REQUIREMENTS = [
     "aiofiles",
     "fastapi>=0.70.0,<=0.106.0",
-    "imjoy-rpc>=0.5.44",
+    "imjoy-rpc>=0.5.57",
     "msgpack>=1.0.2",
     "numpy",
     "pydantic[email]>=2.6.1",
