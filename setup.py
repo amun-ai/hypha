0a75e7ccc7ad00823cd3eb44473207c1f69f1dde
Wei Ouyang <oeway007@gmail.com>
2024-08-13 23:03:09 -0700

    upgrade hypha-rpc

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ DESCRIPTION = (
 REQUIREMENTS = [
     "aiofiles",
     "fastapi>=0.70.0,<=0.106.0",
-    "hypha-rpc>=0.20.17",
+    "hypha-rpc>=0.20.18",
     "msgpack>=1.0.2",
     "numpy",
     "pydantic[email]>=2.6.1",
