2c4a64874887cc65fc5e25e7f7a848ab8cfc9dd2
Wei Ouyang <oeway007@gmail.com>
2024-12-03 13:31:50 -0800

    Fix unload

--- hypha/core/workspace.py
+++ hypha/core/workspace.py
@@ -1751,12 +1751,9 @@ class WorkspaceManager:
             )
             await self._event_bus.emit(f"unload:{ws}", "Unloading workspace: " + ws)
 
-        if winfo.persistent and self._s3_controller:
-            # since the workspace will be persisted, we can remove the workspace info from the redis store
-            await self._redis.hdel("workspaces", ws)
-
-        await self._event_bus.emit("workspace_unloaded", winfo.model_dump())
-        logger.info("Workspace %s unloaded.", ws)
+        # if winfo.persistent and self._s3_controller:
+        #     # since the workspace will be persisted, we can remove the workspace info from the redis store
+        #     await self._redis.hdel("workspaces", ws)
 
         if not winfo.persistent:
             # delete all the items in redis starting with `workspaces_name:`
@@ -1768,6 +1765,9 @@ class WorkspaceManager:
                 await self._s3_controller.cleanup_workspace(winfo)
 
         self._active_ws.dec()
+
+        await self._event_bus.emit("workspace_unloaded", winfo.model_dump())
+        logger.info("Workspace %s unloaded.", ws)
 
     @schema_method
     async def cleanup(
