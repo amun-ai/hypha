96d173e9ff47fe1b5debcbfe1e2ecee77ff0fd80
Wei Ouyang <oeway007@gmail.com>
2025-07-23 22:53:12 -0700

    Fix f-string syntax error in workspace.py
    - Fixed multiline f-string causing SyntaxError: f-string: unmatched '('
    - Changed to single line f-string with proper quote escaping
    - This was preventing all tests from running in CI
    - Critical fix for Python syntax compliance

--- hypha/core/workspace.py
+++ hypha/core/workspace.py
@@ -1950,9 +1950,7 @@ class WorkspaceManager:
             assert service_api, f"Failed to get service: {svc_info.id}"
             workspace = svc_info.id.split("/")[0]
             service_api["config"]["workspace"] = workspace
-            service_api["config"][
-                "url"
-            ] = f"{self._server_info['public_base_url']}/{workspace}/services/{svc_info.id.split("/")[-1]}"
+            service_api["config"]["url"] = f"{self._server_info['public_base_url']}/{workspace}/services/{svc_info.id.split('/')[-1]}"
             return service_api
         except KeyError as exp:
             if "@" in service_id:
